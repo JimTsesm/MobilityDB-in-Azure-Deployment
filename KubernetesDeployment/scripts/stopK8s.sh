163d8e4581f29baff3d8cc039f0eda285888142c
Dimitris Tsesmelis <zas1122@hotmail.com>
2021-04-12 12:21:06 +0300

    jk

--- KubernetesDeployment/scripts/stopK8s.sh
+++ KubernetesDeployment/scripts/stopK8s.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo kubectl patch pvc postgres-pv-claim -p '{"metadata":{"finalizers": []}}' --type=merge
+sudo kubectl patch pv postgres-pv-volume-coordinator -p '{"metadata":{"finalizers": []}}' --type=merge
+
 sudo kubectl delete service citus-master 
 sudo kubectl delete service citus-workers
 sudo kubectl delete deployment citus-master
@@ -9,4 +12,3 @@ sudo kubectl delete persistentvolumeclaim postgres-pv-claim-coordinator
 sudo kubectl delete persistentvolume postgres-pv-volume-coordinator
 sudo kubectl delete secret postgresql-secrets
 sudo kubectl delete secret postgres-secrets-params
-
